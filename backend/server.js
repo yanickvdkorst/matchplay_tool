90a8a8a5fb8bc6356c3b18fab3041b4550ac1cf1
Yanick van der Korst <yanick.van.der.korst@kpnmail.nl>
2025-10-29 15:17:48 +0100

    new db

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false } 
 });
 
 // ROUTE: nieuwe speler toevoegen
