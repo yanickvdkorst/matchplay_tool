f9a98a2892ca0dc98b771a9431af17a3a57a9bf1
Yanick van der Korst <yanick.van.der.korst@kpnmail.nl>
2025-11-24 12:02:26 +0100

    Added tags

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false } 
+  // ssl: { rejectUnauthorized: false } 
 });
 
 
