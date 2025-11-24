bcf56e3428bf1767a893a2c7e18d55c57c5faccc
Yanick van der Korst <yanick.van.der.korst@kpnmail.nl>
2025-11-24 15:42:56 +0100

    Styling changes to cloudflare

--- frontend/script.js
+++ frontend/script.js
@@ -45,15 +45,12 @@ function setLoading(isLoading) {
 
 // Cloudflare
  function onTurnstileSuccess(token) {
-    console.log("Turnstile success:", token);
     document.getElementById("addMatchBtn").disabled = false;
   }
   function onTurnstileError(errorCode) {
-    console.error("Turnstile error:", errorCode);
     document.getElementById("addMatchBtn").disabled = true;
   }
   function onTurnstileExpired() {
-    console.warn("Turnstile token expired");
     document.getElementById("addMatchBtn").disabled = true;
   }
 
